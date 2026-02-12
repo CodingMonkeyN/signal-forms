fa7076407c6499d2aa00f4ff0d2b1c2191d507bd
CodingMonkeyN <nils.gabriel.braun@gmail.com>
2026-02-12 20:06:24 +0100

    remove hash

--- apps/app/src/app/app.config.ts
+++ apps/app/src/app/app.config.ts
@@ -2,12 +2,9 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(appRoutes, withHashLocation()),
-  ],
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes)],
 };
